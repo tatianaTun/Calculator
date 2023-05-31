23f9cae787f1ea2b19110b2539e5ca703c5b3d80
Tatiana <123013751+tatianaTun@users.noreply.github.com>
2023-05-31 11:47:30 -0700

    Add files via upload

---
+++ Calculator/Calculator/Form1.Designer.vb
@@ -0,0 +1,318 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Equal = New Button()
+        Division = New Button()
+        Multiplication = New Button()
+        Substraction = New Button()
+        Addtion = New Button()
+        Clear = New Button()
+        RichTextBox1 = New RichTextBox()
+        Delete = New Button()
+        RichTextBox2 = New RichTextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.MediumPurple
+        Button1.Font = New Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Button1.Location = New Point(29, 288)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(74, 55)
+        Button1.TabIndex = 0
+        Button1.Text = "7"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.MediumPurple
+        Button2.Font = New Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Button2.Location = New Point(118, 288)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(74, 55)
+        Button2.TabIndex = 1
+        Button2.Text = "8"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.MediumPurple
+        Button3.Font = New Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Button3.Location = New Point(207, 288)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(74, 55)
+        Button3.TabIndex = 2
+        Button3.Text = "9"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.MediumPurple
+        Button4.Font = New Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Button4.Location = New Point(29, 372)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(74, 55)
+        Button4.TabIndex = 3
+        Button4.Text = "4"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.BackColor = Color.MediumPurple
+        Button5.Font = New Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Button5.Location = New Point(118, 372)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(74, 55)
+        Button5.TabIndex = 4
+        Button5.Text = "5"
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' Button6
+        ' 
+        Button6.BackColor = Color.MediumPurple
+        Button6.Font = New Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Button6.Location = New Point(207, 372)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(74, 55)
+        Button6.TabIndex = 5
+        Button6.Text = "6"
+        Button6.UseVisualStyleBackColor = False
+        ' 
+        ' Button7
+        ' 
+        Button7.BackColor = Color.MediumPurple
+        Button7.Font = New Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Button7.Location = New Point(29, 457)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(74, 55)
+        Button7.TabIndex = 6
+        Button7.Text = "1"
+        Button7.UseVisualStyleBackColor = False
+        ' 
+        ' Button8
+        ' 
+        Button8.BackColor = Color.MediumPurple
+        Button8.Font = New Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Button8.Location = New Point(118, 457)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(74, 55)
+        Button8.TabIndex = 7
+        Button8.Text = "2"
+        Button8.UseVisualStyleBackColor = False
+        ' 
+        ' Button9
+        ' 
+        Button9.BackColor = Color.MediumPurple
+        Button9.Font = New Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Button9.Location = New Point(207, 457)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(74, 55)
+        Button9.TabIndex = 8
+        Button9.Text = "3"
+        Button9.UseVisualStyleBackColor = False
+        ' 
+        ' Button10
+        ' 
+        Button10.BackColor = Color.MediumPurple
+        Button10.Font = New Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Button10.Location = New Point(29, 541)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(74, 55)
+        Button10.TabIndex = 9
+        Button10.Text = "."
+        Button10.UseVisualStyleBackColor = False
+        ' 
+        ' Button11
+        ' 
+        Button11.BackColor = Color.MediumPurple
+        Button11.Font = New Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Button11.Location = New Point(118, 541)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(163, 55)
+        Button11.TabIndex = 10
+        Button11.Text = "0"
+        Button11.UseVisualStyleBackColor = False
+        ' 
+        ' Equal
+        ' 
+        Equal.BackColor = Color.DodgerBlue
+        Equal.Font = New Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Equal.Location = New Point(301, 528)
+        Equal.Name = "Equal"
+        Equal.Size = New Size(94, 68)
+        Equal.TabIndex = 11
+        Equal.Text = "="
+        Equal.UseVisualStyleBackColor = False
+        ' 
+        ' Division
+        ' 
+        Division.BackColor = Color.DarkSlateBlue
+        Division.Font = New Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Division.Location = New Point(301, 200)
+        Division.Name = "Division"
+        Division.Size = New Size(94, 62)
+        Division.TabIndex = 12
+        Division.Text = "/"
+        Division.UseVisualStyleBackColor = False
+        ' 
+        ' Multiplication
+        ' 
+        Multiplication.BackColor = Color.DarkSlateBlue
+        Multiplication.Font = New Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Multiplication.Location = New Point(301, 281)
+        Multiplication.Name = "Multiplication"
+        Multiplication.Size = New Size(94, 62)
+        Multiplication.TabIndex = 13
+        Multiplication.Text = "x"
+        Multiplication.UseVisualStyleBackColor = False
+        ' 
+        ' Substraction
+        ' 
+        Substraction.BackColor = Color.DarkSlateBlue
+        Substraction.Font = New Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Substraction.Location = New Point(301, 365)
+        Substraction.Name = "Substraction"
+        Substraction.Size = New Size(94, 62)
+        Substraction.TabIndex = 14
+        Substraction.Text = "-"
+        Substraction.UseVisualStyleBackColor = False
+        ' 
+        ' Addtion
+        ' 
+        Addtion.BackColor = Color.DarkSlateBlue
+        Addtion.Font = New Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point)
+        Addtion.Location = New Point(301, 450)
+        Addtion.Name = "Addtion"
+        Addtion.Size = New Size(94, 62)
+        Addtion.TabIndex = 15
+        Addtion.Text = "+"
+        Addtion.UseVisualStyleBackColor = False
+        ' 
+        ' Clear
+        ' 
+        Clear.BackColor = Color.DarkSlateBlue
+        Clear.Font = New Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point)
+        Clear.Location = New Point(29, 200)
+        Clear.Name = "Clear"
+        Clear.Size = New Size(163, 62)
+        Clear.TabIndex = 16
+        Clear.Text = "CLEAR"
+        Clear.UseVisualStyleBackColor = False
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.BackColor = Color.FromArgb(CByte(0), CByte(64), CByte(64))
+        RichTextBox1.BorderStyle = BorderStyle.None
+        RichTextBox1.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        RichTextBox1.ForeColor = Color.White
+        RichTextBox1.Location = New Point(29, 92)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(366, 78)
+        RichTextBox1.TabIndex = 17
+        RichTextBox1.Text = "0"' 
+        ' Delete
+        ' 
+        Delete.BackColor = Color.DarkSlateBlue
+        Delete.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Delete.Location = New Point(207, 200)
+        Delete.Name = "Delete"
+        Delete.Size = New Size(74, 62)
+        Delete.TabIndex = 18
+        Delete.Text = "DELETE"
+        Delete.UseVisualStyleBackColor = False
+        ' 
+        ' RichTextBox2
+        ' 
+        RichTextBox2.BackColor = Color.FromArgb(CByte(0), CByte(64), CByte(64))
+        RichTextBox2.ForeColor = Color.White
+        RichTextBox2.Location = New Point(29, 37)
+        RichTextBox2.Name = "RichTextBox2"
+        RichTextBox2.Size = New Size(366, 59)
+        RichTextBox2.TabIndex = 19
+        RichTextBox2.Text = ""' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.SlateBlue
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        ClientSize = New Size(423, 649)
+        Controls.Add(RichTextBox2)
+        Controls.Add(Delete)
+        Controls.Add(RichTextBox1)
+        Controls.Add(Clear)
+        Controls.Add(Addtion)
+        Controls.Add(Substraction)
+        Controls.Add(Multiplication)
+        Controls.Add(Division)
+        Controls.Add(Equal)
+        Controls.Add(Button11)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Equal As Button
+    Friend WithEvents Division As Button
+    Friend WithEvents Multiplication As Button
+    Friend WithEvents Substraction As Button
+    Friend WithEvents Addtion As Button
+    Friend WithEvents Clear As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Delete As Button
+    Friend WithEvents RichTextBox2 As RichTextBox
+End Class
---
+++ Calculator/Calculator/Form1.vb
@@ -0,0 +1,240 @@
+﻿Public Class Form1
+
+
+    '''''''   VARIABLES   '''''''
+
+    Dim storage As Double = 0 'Double here so can store numbers with decimal
+    Dim arithOperator As String = ""
+    Dim result As Double
+
+
+    '''''''   FUNCTIONS   '''''''
+
+    Sub Calculate()  ' we create a function(subroutine) 
+        If arithOperator = "+" Then
+            result = storage + Convert.ToDouble(RichTextBox1.Text)
+        ElseIf arithOperator = "-" Then
+            result = storage - Convert.ToDouble(RichTextBox1.Text)
+        ElseIf arithOperator = "*" Then
+            result = storage * Convert.ToDouble(RichTextBox1.Text)
+        ElseIf arithOperator = "/" Then
+            result = storage / Convert.ToDouble(RichTextBox1.Text)
+        Else
+            result = storage 'QUESTION - Not sure about this part, should I keep it?
+        End If
+        arithOperator = "="
+        RichTextBox1.Text = result.ToString()
+        storage = result
+    End Sub
+
+
+    '''''''   NUMBERS BUTTONS   '''''''
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        If RichTextBox1.Text = "0" Then
+            RichTextBox1.Text = "0"
+        ElseIf RichTextBox1.Text = Convert.ToString(result) Then
+            RichTextBox1.Text = "0"
+        ElseIf Not RichTextBox1.Text = "0" And Not arithOperator = "" Then
+            RichTextBox1.Text += "0"
+        Else
+            RichTextBox1.Text += "0"
+        End If
+        RichTextBox2.Text += "0"
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If RichTextBox1.Text = "0" Then
+            RichTextBox1.Text = "1"
+        ElseIf RichTextBox1.Text = Convert.ToString(result) Then
+            RichTextBox1.Text = "1"
+        ElseIf Not RichTextBox1.Text = "0" And Not arithOperator = "" Then
+            RichTextBox1.Text += "1"
+        Else
+            RichTextBox1.Text += "1" 'Do I need this part?
+        End If
+        RichTextBox2.Text += "1"
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If RichTextBox1.Text = "0" Then
+            RichTextBox1.Text = "2"
+        ElseIf RichTextBox1.Text = Convert.ToString(result) Then
+            RichTextBox1.Text = "2"
+        ElseIf Not RichTextBox1.Text = "0" And Not arithOperator = "" Then
+            RichTextBox1.Text += "2"
+        Else
+            RichTextBox1.Text += "2"
+        End If
+        RichTextBox2.Text += "2"
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If RichTextBox1.Text = "0" Then
+            RichTextBox1.Text = "3"
+        ElseIf RichTextBox1.Text = Convert.ToString(result) Then
+            RichTextBox1.Text = "3"
+        ElseIf Not RichTextBox1.Text = "0" And Not arithOperator = "" Then
+            RichTextBox1.Text += "3"
+        Else
+            RichTextBox1.Text += "3"
+        End If
+        RichTextBox2.Text += "3"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If RichTextBox1.Text = "0" Then
+            RichTextBox1.Text = "4"
+        ElseIf RichTextBox1.Text = Convert.ToString(result) Then
+            RichTextBox1.Text = "4"
+        ElseIf Not RichTextBox1.Text = "0" And Not arithOperator = "" Then
+            RichTextBox1.Text += "4"
+        Else
+            RichTextBox1.Text += "4"
+        End If
+        RichTextBox2.Text += "4"
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If RichTextBox1.Text = "0" Then
+            RichTextBox1.Text = "5"
+        ElseIf RichTextBox1.Text = Convert.ToString(result) Then
+            RichTextBox1.Text = "5"
+        ElseIf Not RichTextBox1.Text = "0" And Not arithOperator = "" Then
+            RichTextBox1.Text += "5"
+        Else
+            RichTextBox1.Text += "5"
+        End If
+        RichTextBox2.Text += "5"
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If RichTextBox1.Text = "0" Then
+            RichTextBox1.Text = "6"
+        ElseIf RichTextBox1.Text = Convert.ToString(result) Then
+            RichTextBox1.Text = "6"
+        ElseIf Not RichTextBox1.Text = "0" And Not arithOperator = "" Then
+            RichTextBox1.Text += "6"
+        Else
+            RichTextBox1.Text += "6"
+        End If
+        RichTextBox2.Text += "6"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If RichTextBox1.Text = "0" Then
+            RichTextBox1.Text = "7"
+        ElseIf RichTextBox1.Text = Convert.ToString(result) Then
+            RichTextBox1.Text = "7"
+        ElseIf Not RichTextBox1.Text = "0" And Not arithOperator = "" Then
+            RichTextBox1.Text += "7"
+        Else
+            RichTextBox1.Text += "7"
+        End If
+        RichTextBox2.Text += "7"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If RichTextBox1.Text = "0" Then
+            RichTextBox1.Text = "8"
+        ElseIf RichTextBox1.Text = Convert.ToString(result) Then
+            RichTextBox1.Text = "8"
+        ElseIf Not RichTextBox1.Text = "0" And Not arithOperator = "" Then
+            RichTextBox1.Text += "8"
+        Else
+            RichTextBox1.Text += "8"
+        End If
+        RichTextBox2.Text += "8"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If RichTextBox1.Text = "0" Then
+            RichTextBox1.Text = "9"
+        ElseIf RichTextBox1.Text = Convert.ToString(result) Then
+            RichTextBox1.Text = "9"
+        ElseIf Not RichTextBox1.Text = "0" And Not arithOperator = "" Then
+            RichTextBox1.Text += "9"
+        Else
+            RichTextBox1.Text += "9"
+        End If
+        RichTextBox2.Text += "9"
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        If Not RichTextBox1.Text.Contains(".") Then
+            RichTextBox1.Text += "."
+            RichTextBox2.Text += "."
+        End If
+
+    End Sub
+
+
+    '''''''   OPERATIONS BUTTONS   '''''''
+
+    Private Sub Clear_Click(sender As Object, e As EventArgs) Handles Clear.Click
+        RichTextBox1.Clear()
+        RichTextBox2.Clear()
+        storage = 0
+        arithOperator = ""
+
+    End Sub
+
+    Private Sub Delete_Click(sender As Object, e As EventArgs) Handles Delete.Click
+        If RichTextBox1.Text <> "" Then
+            RichTextBox1.Text = RichTextBox1.Text.Substring(0, RichTextBox1.TextLength - 1)
+            RichTextBox2.Text = RichTextBox2.Text.Substring(0, RichTextBox2.TextLength - 1)
+        End If
+        'Another way for demonstration
+        'Dim textLength As Integer = RichTextBox1.TextLength - 1
+        'Dim textboxText As String = RichTextBox1.Text
+        'RichTextBox1.Text = textboxText.Substring(0, textLength)
+    End Sub
+
+    Private Sub Addtion_Click(sender As Object, e As EventArgs) Handles Addtion.Click
+        If arithOperator <> "" Then
+            Calculate()
+        Else
+            storage = Convert.ToDouble(RichTextBox1.Text)
+            RichTextBox1.Clear()
+        End If
+        arithOperator = "+"
+        RichTextBox2.Text += "+"
+    End Sub
+
+    Private Sub Substraction_Click(sender As Object, e As EventArgs) Handles Substraction.Click
+        If arithOperator <> "" Then
+            Calculate()
+        Else
+            storage = Convert.ToDouble(RichTextBox1.Text)
+            RichTextBox1.Clear()
+        End If
+        arithOperator = "-"
+        RichTextBox2.Text += "-"
+    End Sub
+
+    Private Sub Multiplication_Click(sender As Object, e As EventArgs) Handles Multiplication.Click
+        If arithOperator <> "" Then
+            Calculate()
+        Else
+            storage = Convert.ToDouble(RichTextBox1.Text)
+            RichTextBox1.Clear()
+        End If
+        arithOperator = "*"
+        RichTextBox2.Text += "*"
+    End Sub
+
+    Private Sub Division_Click(sender As Object, e As EventArgs) Handles Division.Click
+        If arithOperator <> "" Then
+            Calculate()
+        Else
+            storage = Convert.ToDouble(RichTextBox1.Text)
+            RichTextBox1.Clear()
+        End If
+        arithOperator = "/"
+        RichTextBox2.Text += "/"
+    End Sub
+
+    Private Sub Equal_Click(sender As Object, e As EventArgs) Handles Equal.Click
+        Calculate() 'calls for the function I created at the top
+    End Sub
+End Class
